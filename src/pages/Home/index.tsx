adaacc65f03d8369ea17f9aa7451c7dd1e2320a8
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2020-11-07 21:15:28 -0300

    resolvendo bug na paginaçao

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { Container, Paginate } from './styles';
 import { api, apiKey } from '../../services/api';
 import Movie from '../../components/Movie';
 import { Creators as CreatorsMessage } from '../../store/ducks/message';
+
+const LEFT_PAGE = 'LEFT';
+const RIGHT_PAGE = 'RIGHT';
 
 interface MovieDetails {
   id: number;
@@ -17,12 +20,25 @@ interface MovieDetails {
   genre_ids: [];
 }
 
+const range = (from: any, to: any, step = 1) => {
+  let i = from;
+  const rangeData = [];
+
+  while (i <= to) {
+    rangeData.push(i);
+    i += step;
+  }
+
+  return rangeData;
+};
+
 const Home = () => {
   const [search, setSearch] = useState('');
   const [genres, setGenres] = useState();
   const [pageNow, setPage] = useState('1');
   const [movies, setMovies] = useState([]);
-  const [totalPage, setTotalPage] = useState();
+  const [pageNeighbours] = useState(1);
+  const [data, setData] = useState();
 
   const dispatch = useDispatch();
 
@@ -37,20 +53,15 @@ const Home = () => {
         `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=pt-BR`
       );
 
+      setData(response.data);
       setGenres(generes.data);
-      const { page, results } = response.data;
-
-      const indexOfLastTodo = page * 5;
+      // eslint-disable-next-line camelcase
+      const { results } = response.data;
+      const indexOfLastTodo = 5;
       const indexOfFirstTodo = indexOfLastTodo - 5;
-      const currentTodos = results.slice(indexOfFirstTodo, indexOfLastTodo);
+      const currentMovies = results.slice(indexOfFirstTodo, indexOfLastTodo);
 
-      const pageNumbers = [];
-      for (let i = 1; i <= Math.ceil(results.length / 5); i++) {
-        pageNumbers.push(i);
-      }
-
-      setTotalPage(pageNumbers);
-      setMovies(currentTodos);
+      setMovies(currentMovies);
     } catch (e) {
       dispatch(
         CreatorsMessage.messageFailure(
@@ -63,6 +74,56 @@ const Home = () => {
   useEffect(() => {
     onHandleSubmit();
   }, [search, pageNow, onHandleSubmit]);
+
+  const fetchPageNumbers = useCallback(() => {
+    if (data) {
+      // eslint-disable-next-line camelcase
+      const { page, total_pages } = data;
+
+      const totalPages = Math.ceil(total_pages / 5);
+
+      const totalNumbers = pageNeighbours * 2 + 3;
+      const totalBlocks = totalNumbers + 2;
+
+      if (totalPages > totalBlocks) {
+        const startPage = Math.max(2, page - pageNeighbours);
+        const endPage = Math.min(totalPages - 1, page + pageNeighbours);
+        let pages = range(startPage, endPage);
+
+        const hasLeftSpill = startPage > 2;
+        const hasRightSpill = totalPages - endPage > 1;
+        const spillOffset = totalNumbers - (pages.length + 1);
+
+        switch (true) {
+          // handle: (1) < {5 6} [7] {8 9} (10)
+          case hasLeftSpill && !hasRightSpill: {
+            const extraPages = range(startPage - spillOffset, startPage - 1);
+            pages = [LEFT_PAGE, ...extraPages, ...pages];
+            break;
+          }
+
+          // handle: (1) {2 3} [4] {5 6} > (10)
+          case !hasLeftSpill && hasRightSpill: {
+            const extraPages = range(endPage + 1, endPage + spillOffset);
+            pages = [...pages, ...extraPages, RIGHT_PAGE];
+            break;
+          }
+
+          // handle: (1) < {4 5} [6] {7 8} > (10)
+          case hasLeftSpill && hasRightSpill:
+          default: {
+            pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
+            break;
+          }
+        }
+        // return [1, ...pages, totalPages];
+        return [1, ...pages, totalPages];
+
+        // return range(1, totalPages);
+      }
+      return range(1, totalPages);
+    }
+  }, [data, pageNeighbours]);
 
   function handleInputChange(event: React.FormEvent<HTMLInputElement>): void {
     const { value } = event.currentTarget;
@@ -78,7 +139,10 @@ const Home = () => {
     pageItem?.classList.add('active');
     setPage(n);
     onHandleSubmit();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
+
+  const pages = useMemo(() => fetchPageNumbers(), [fetchPageNumbers]);
 
   return (
     <Container>
@@ -98,12 +162,23 @@ const Home = () => {
       ))}
 
       <Paginate>
-        {totalPage !== undefined &&
-          totalPage.map((number: string, index: number) => (
-            <div key={number} id={number}>
-              <li onClick={() => setPaginate(number)}>{number}</li>
-            </div>
-          ))}
+        {pages !== undefined &&
+          pages.map((number: string, index: number) => {
+            if (number === LEFT_PAGE) return <React.Fragment key={number} />;
+
+            if (number === RIGHT_PAGE) return <React.Fragment key={number} />;
+
+            return (
+              <div
+                className={`${pageNow == number ? 'active' : ''}`}
+                id={number}
+                key={number}
+                onClick={() => setPaginate(number)}
+              >
+                <li>{number}</li>
+              </div>
+            );
+          })}
       </Paginate>
     </Container>
   );
